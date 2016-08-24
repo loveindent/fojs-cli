ea44486f701ca68bdf6fbba1549ca0f066607ef4
Breizhad <sebastien.iquel@gmail.com>
2016-08-24 15:24:39 +0200

    ftr(delete) remove a component

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var program = require('commander');
 program
   .version('0.0.1')
   .command('create [ressource]', 'create something')
+  .command('delete [ressource]', 'delete something')
 ;
 
 program.parse(process.argv);
