59b1dafe8ee6c650550ddb5e79bc8d88cd5c4789
Breizhad <sebastien.iquel@gmail.com>
2016-09-03 16:28:08 +0200

    Add compile method

--- fo-start.js
+++ fo-start.js
@@ -1,14 +1,17 @@
-var program = require('commander');
-
-var startApp = require('./lib/start-app');
+var program = require('commander')
+var _ = require('lodash')
+var startApp = require('./lib/start-app')
 
 program
-  .command('app')
+  .command('app [name]')
   .description('start app')
-  .action(function() {
-    console.log('Boostraping app is started');
-    startApp()
+  .action(function(name) {
+    if (_.isEmpty(name)) {
+      console.error('Doh! You must give a name to your application');
+      process.exit(1);
+    }
+    console.log('Boostraping application is started');
+    startApp(name)
   })
-;
 
-program.parse(process.argv);
+program.parse(process.argv)
